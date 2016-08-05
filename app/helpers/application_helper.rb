47c76cc6410b3815867cac4953338a06eb38dbe6
Nelma42 <manal.tab.42@gmail.com>
2016-08-05 16:37:55 +0200

    snack img + debut de css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	def gravatar_for(user)
-		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-		gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=150"
-		image_tag(gravatar_url, alt: user.email)
+			gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+			gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=150"
+			image_tag(gravatar_url, alt: user.email)
 	end
 end
